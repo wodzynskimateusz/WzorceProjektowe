cd654b88c394e389c5cafadca175130731f99a3c
Unknown <wodzynski.mateusz@gmail.com>
2018-03-04 15:26:58 +0100

    Założenie repo

---
+++ src/WzorzecAdapter/KaczoIndyk/DzikaKaczka.java
@@ -0,0 +1,14 @@
+package WzorzecAdapter.KaczoIndyk;
+
+public class DzikaKaczka implements Kaczka {
+
+    @Override
+    public void kwacz() {
+        System.out.println("Kwa kwa");
+    }
+
+    @Override
+    public void lec() {
+        System.out.println("Fru fru");
+    }
+}
